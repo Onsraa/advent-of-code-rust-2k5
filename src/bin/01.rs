09d72f40b418ebda17590948568ad30760a829dd
Teddy <teddytruong@protonmail.com>
2025-12-08 10:40:47 -0500

    solved : day 1 part 1
    best time : 296.8µs
    average time : 825,7µs
    
    dhat: Total: 0 bytes in 0 blocks
    dhat: At t-gmax: 0 bytes in 0 blocks
    dhat: At t-end: 0 bytes in 0 blocks

---
+++ src/bin/01.rs
@@ -0,0 +1,56 @@
+advent_of_code::solution!(1);
+
+fn parse_line(line: &str) -> Option<(char, &str)> {
+    let mut chars = line.chars();
+    let first_char = chars.next()?;
+    let rest = &line[first_char.len_utf8()..];
+    Some((first_char, rest))
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+
+    let mut dial: i32 = 50;
+    let mut counter: u64= 0;
+
+    for line in input.lines() {
+        if let Some((direction, number)) = parse_line(line) {
+            if let Ok(value) = number.parse::<u32>() {
+                match direction {
+                    'L' => {
+                        dial = ((dial - value as i32) % 100 + 100) % 100;
+                    },
+                    'R' => {
+                        dial = ((dial + value as i32) % 100 + 100) % 100
+                    },
+                    _ => {return None}
+                }
+                if dial == 0 {
+                    counter += 1
+                }
+            }
+        }
+    }
+
+    Some(counter)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
